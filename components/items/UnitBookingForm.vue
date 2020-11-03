5b04f5b3e4c64f120ef5c7cfee470345b8bd0a8f
Edmund Hee <edmund.hee05@gmail.com>
2020-11-04 00:38:18 +0800

    Setup firebase and submit reservation data

--- components/items/UnitBookingForm.vue
+++ components/items/UnitBookingForm.vue
@@ -1,114 +1,180 @@
 <template>
-    <div class="px-2">
-        <div id="trigger"></div>
-        <div id="bookingForm" class="border border-gray-400 rounded-lg shadow-2xl px-6 py-4">
-            <div class="flex flex-col">
-                <div class="text-xl py-6">
-                    RM180 <span class="text-sm font-light">/ night</span>
-                </div>
-                <div class="grid grid-rows-2 rounded-lg border border-gray-600 my-4">
-                    <div class="grid grid-cols-2">
-                        <div class="flex flex-col py-2 px-3 border-r border-gray-600">
-                            <div class="text-xs font-semibold tracking-wide">CHECK-IN</div>
-                            <input type="date" class="border-none focus:border-none active:border-none text-sm py-1 font-light" v-model="dateFrom"/>
-                        </div>
-                        <div class="flex flex-col py-2 px-3">
-                            <div class="text-xs font-semibold tracking-wide">CHECK-OUT</div>
-                            <input type="date" class="border-none focus:border-none active:border-none text-sm py-1 font-light" v-model="dateTo"/>
-                        </div>
-                    </div>
-                    <div class="flex items-center justify-between border-t border-gray-600">
-                        <div class="flex flex-col py-2 px-3 w-full">
-                            <div class="text-xs font-medium">GUESTS</div>
-                            <select class="border-none focus:outline-none active:outline-none py-1"
-                                v-model="guest"
-                                @select="selectGuest(guest)"
-                                >
-                                <option disabled value="" selected>Please select one</option>
-                                <option
-                                    v-for="guest in dropDown" :key="guest" :value="guest">
-                                        {{ guest }}
-                                </option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-                <div id="cancellationPolicies" class="flex flex-col my-4">
-                    <div class="text-xs font-medium">CANCELLATION POLICIES</div>
-                    <div class="grid grid-rows-2 rounded-lg border border-gray-600">
-                        <div class="flex items-center justify-between border-b border-gray-600">
-                            <div class="flex flex-col py-2 px-3">
-                                <div class="py-1 font-light">
-                                    Non-refundable · RM1,080 total {{policy1}}
-                                </div>
-                                <div class="text-xs text-gray-700">
-                                    Cancel before 3:00 PM on Oct 16 and only get a refund of the cleaning fee.
-                                </div>
-                            </div>
-                            <div class="px-4">
-                                <input type="radio" class="form-radio h-6 w-6 text-gray-600" v-model="policy1" @click="policy2 = false; policy1=true"/>
-                            </div>
-                        </div>
-                        <div class="flex items-center justify-between">
-                            <div class="flex flex-col py-2 px-3">
-                                <div class="py-1 font-light">
-                                    Refundable · RM1,090 total {{policy2}}
-                                </div>
-                                <div class="text-xs text-gray-700">
-                                    Free cancellation until Oct 11. After that, cancel before 3:00 PM on Oct 16 and get a 50% refund, minus the first night and service fee.
-                                </div>
-                            </div>
-                            <div class="px-4">
-                                <input type="radio" class="form-radio h-6 w-6 text-gray-600" v-model="policy2" @click="policy1 = false; policy2=true"/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <button type="button" class="transition-all duration-500 ease-in-out text-white bg-red-600 py-3 px-4 rounded-lg hover:bg-red-700" @click="reserve">Reserve</button>
-                
-                <div class="border-b border-gray-400 py-4 text-center font-light text-sm">
-                    You won't be charged yet
-                </div>
-                <div class="flex items-center justify-between pt-6 pb-4">
-                    <div class="text-lg font-medium">Total</div>
-                    <div class="text-lg font-medium">RM1,080</div>
-                </div>
-            </div>
+  <div class="px-2">
+    <div id="trigger"></div>
+    <div
+      id="bookingForm"
+      class="border border-gray-400 rounded-lg shadow-2xl px-6 py-4"
+    >
+      <div class="flex flex-col">
+        <div class="text-xl py-6">
+          RM180 <span class="text-sm font-light">/ night</span>
         </div>
+        <div class="grid grid-rows-2 rounded-lg border border-gray-600 my-4">
+          <div class="grid grid-cols-2">
+            <div class="flex flex-col py-2 px-3 border-r border-gray-600">
+              <div class="text-xs font-semibold tracking-wide">CHECK-IN</div>
+              <input
+                type="date"
+                class="border-none focus:border-none active:border-none text-sm py-1 font-light"
+                v-model="dateFrom"
+              />
+            </div>
+            <div class="flex flex-col py-2 px-3">
+              <div class="text-xs font-semibold tracking-wide">CHECK-OUT</div>
+              <input
+                type="date"
+                class="border-none focus:border-none active:border-none text-sm py-1 font-light"
+                v-model="dateTo"
+              />
+            </div>
+          </div>
+          <div
+            class="flex items-center justify-between border-t border-gray-600"
+          >
+            <div class="flex flex-col py-2 px-3 w-full">
+              <div class="text-xs font-medium">GUESTS</div>
+              <select
+                class="border-none focus:outline-none active:outline-none py-1"
+                v-model="guest"
+                @select="selectGuest(guest)"
+              >
+                <option disabled value="" selected>Please select one</option>
+                <option v-for="guest in dropDown" :key="guest" :value="guest">
+                  {{ guest }}
+                </option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div id="cancellationPolicies" class="flex flex-col my-4">
+          <div class="text-xs font-medium">CANCELLATION POLICIES</div>
+          <div class="grid grid-rows-2 rounded-lg border border-gray-600">
+            <div
+              class="flex items-center justify-between border-b border-gray-600"
+            >
+              <div class="flex flex-col py-2 px-3">
+                <div class="py-1 font-light">
+                  Non-refundable · RM1,080 total {{ policy1 }}
+                </div>
+                <div class="text-xs text-gray-700">
+                  Cancel before 3:00 PM on Oct 16 and only get a refund of the
+                  cleaning fee.
+                </div>
+              </div>
+              <div class="px-4">
+                <input
+                  type="radio"
+                  class="form-radio h-6 w-6 text-gray-600"
+                  v-model="policy1"
+                  @click="
+                    policy2 = false;
+                    policy1 = true;
+                  "
+                />
+              </div>
+            </div>
+            <div class="flex items-center justify-between">
+              <div class="flex flex-col py-2 px-3">
+                <div class="py-1 font-light">
+                  Refundable · RM1,090 total {{ policy2 }}
+                </div>
+                <div class="text-xs text-gray-700">
+                  Free cancellation until Oct 11. After that, cancel before 3:00
+                  PM on Oct 16 and get a 50% refund, minus the first night and
+                  service fee.
+                </div>
+              </div>
+              <div class="px-4">
+                <input
+                  type="radio"
+                  class="form-radio h-6 w-6 text-gray-600"
+                  v-model="policy2"
+                  @click="
+                    policy1 = false;
+                    policy2 = true;
+                  "
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <button
+          type="button"
+          class="transition-all duration-500 ease-in-out text-white bg-red-600 py-3 px-4 rounded-lg hover:bg-red-700"
+          @click="reserve"
+        >
+          Reserve
+        </button>
+
+        <div
+          class="border-b border-gray-400 py-4 text-center font-light text-sm"
+        >
+          You won't be charged yet
+        </div>
+        <div class="flex items-center justify-between pt-6 pb-4">
+          <div class="text-lg font-medium">Total</div>
+          <div class="text-lg font-medium">RM1,080</div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            dateFrom: "",
-            dateTo: "",
-            guest: "",
-            dropDown : [
-                "2 Guest",
-                "3 Guest",
-                "4 Guest",
-                "5 Guest",
-                "6 Guest",
-                "7 Guest",
-                "8 Guest",
-                "9 Guest",
-                "10 Guest",
-                "11 Guest",
-                "12 Guest",
-                "13 Guest",
-            ],
-            policy1: false,
-            policy2: false
-        }
+  data() {
+    return {
+      dateFrom: "",
+      dateTo: "",
+      guest: "",
+      dropDown: [
+        "2 Guest",
+        "3 Guest",
+        "4 Guest",
+        "5 Guest",
+        "6 Guest",
+        "7 Guest",
+        "8 Guest",
+        "9 Guest",
+        "10 Guest",
+        "11 Guest",
+        "12 Guest",
+        "13 Guest",
+      ],
+      policy1: false,
+      policy2: false,
+    };
+  },
+  methods: {
+    selectGuest(guest) {
+      this.guest = guest;
     },
-    methods: {
-        selectGuest(guest) {
-            this.guest = guest
-        },
-        reserve(){
-            
-        }
-    }
-}
+    reserve() {
+      const reserveData = {
+        date_from: this.dateFrom,
+        date_to: this.dateTo,
+        guest_number: this.guest,
+        policy_1: this.policy1 == null ? true : false,
+        policy_2: this.policy2 == null ? true : false,
+      };
+
+      // Submit Reserver Data into firebase
+      // and store inside a collection name -reservation-
+
+      this.$fire.firestore
+        .collection("reservation")
+        .add(reserveData)
+        .then(function (doc_ref) {
+          // if reserveData is saved inside firebase
+          // create a pop-up saying the success message
+          alert("Reservation submitted successfully!");
+        })
+        .catch(function (error) {
+          // if reserveData didn't get to save into firebae
+          // create a pop-up saying it failed and print out the error msg
+          alert("Error submitting reservation");
+          console.log("Error ", error);
+        });
+    },
+  },
+};
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,65 +1,86 @@
-const firebaseConfig = process.env.FIREBASE_CRED? process.env.FIREBASE_CRED: "{}";
+const firebaseConfig = process.env.FIREBASE_CRED
+  ? process.env.FIREBASE_CRED
+  : "{}";
 export default {
-  mode: 'universal',
+  mode: "universal",
   components: true,
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      ,{ href: 'https://fonts.googleapis.com/css?family=Poppins:100|Poppins:200|Poppins:300|Poppins:400|Poppins:500|Poppins:600|Poppins:700|Poppins:800|Poppins:900', rel: 'stylesheet'}
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        href:
+          "https://fonts.googleapis.com/css?family=Poppins:100|Poppins:200|Poppins:300|Poppins:400|Poppins:500|Poppins:600|Poppins:700|Poppins:800|Poppins:900",
+        rel: "stylesheet"
+      }
     ],
     script: [
-      { src: 'https://kit.fontawesome.com/48d20e9c42.js', crossorigin: 'anonymous', type:'text/javascript'}
-      ,{ src: 'http://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js', crossorigin: 'anonymous', type: 'text/javascript'}
-      ,{ src: 'http://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js', crossorigin: 'anonymous', type: 'text/javascript'}
+      {
+        src: "https://kit.fontawesome.com/48d20e9c42.js",
+        crossorigin: "anonymous",
+        type: "text/javascript"
+      },
+      {
+        src:
+          "http://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js",
+        crossorigin: "anonymous",
+        type: "text/javascript"
+      },
+      {
+        src:
+          "http://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js",
+        crossorigin: "anonymous",
+        type: "text/javascript"
+      }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss"
   ],
   /*
-  ** Nuxt.js modules
-  */
- modules: [
-    '@nuxtjs/axios',
+   ** Nuxt.js modules
+   */
+  modules: [
+    "@nuxtjs/axios",
     [
-      '@nuxtjs/firebase',
+      "@nuxtjs/firebase",
       {
         config: {
-          apiKey: '<apiKey>',
-          authDomain: '<authDomain>',
-          databaseURL: '<databaseURL>',
-          projectId: '<projectId>',
-          storageBucket: '<storageBucket>',
-          messagingSenderId: '<messagingSenderId>',
-          appId: '<appId>',
-          measurementId: '<measurementId>'
+          apiKey: "AIzaSyAOuwGw5gLEOq7blocYOejlx26cXVTn0HA",
+          authDomain: "webdevseries2.firebaseapp.com",
+          databaseURL: "https://webdevseries2.firebaseio.com",
+          projectId: "webdevseries2",
+          storageBucket: "webdevseries2.appspot.com",
+          messagingSenderId: "53306314880",
+          appId: "1:53306314880:web:b21220e8ed90c04adbd665"
         },
         services: {
           firestore: true
@@ -72,13 +93,12 @@ export default {
     // proxyHeaders: false
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
